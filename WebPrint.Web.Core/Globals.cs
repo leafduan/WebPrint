52abd19a53cfd89ee694a144242406b7aa7fc749
ranmofang <shine8rain@gmail.com>
2014-12-08 16:22:19 +0800

    Update Globals.cs

--- WebPrint.Web.Core/Globals.cs
+++ WebPrint.Web.Core/Globals.cs
@@ -3,7 +3,7 @@
     public sealed class Globals
     {
         /* 3DES IV and Key */
-        public static readonly string DEFAULT_IV = "25316842";
-        public static readonly string DEFAULT_KEY = "CREATED BY R-PAC SZ LEAF";
+        public static readonly string DEFAULT_IV = "12345678";
+        public static readonly string DEFAULT_KEY = "CREATED BY G-HUB SZ LEAF";
     }
 }
